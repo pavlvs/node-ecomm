f9794b19aa92384ed933918c7a3b27cd4a48b9b3
pavlvs <pavlvsxavier@gmail.com>
2021-09-16 09:14:59 +0100

    displayed form

--- index.js
+++ index.js
@@ -2,5 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) =>
+    res.send(/*HTML*/ `
+        <div>
+            <form method="POST" action="">
+                <input name="email" placeholder="email"/>
+                <input name="password" placeholder="password" />
+                <input name="passwordConfirmation" placeholder="password confirmation" />
+                <button>Sign up</button>
+            </form>
+        </div>
+
+    `)
+)
+
+app.post('/', function (req, res) {
+    res.send('Account created!')
+})
+
 app.listen(port, () => console.log(`Example app listening on port port!`))
