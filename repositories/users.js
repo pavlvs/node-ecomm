6b5c49b6616a05fc0ece61e6804350f98ce2f18a
pavlvs <pavlvsxavier@gmail.com>
2021-09-16 14:15:08 +0100

    opened and read users repository

--- repositories/users.js
+++ repositories/users.js
@@ -12,6 +12,25 @@ class UsersRepository {
             fs.writeFileSync(this.filename, '[]')
         }
     }
+
+    async getAll() {
+        // Open the file called this.filename
+        const contents = await fs.promises.readFile(this.filename, {
+            encoding: 'utf8',
+        })
+        // Read its contents
+        console.log(contents)
+
+        // parse the contents
+
+        // return the parsed data
+    }
 }
 
-const repo = new UsersRepository('users.json')
+const test = async (params) => {
+    const repo = new UsersRepository('users.json')
+
+    await repo.getAll()
+}
+
+test()
