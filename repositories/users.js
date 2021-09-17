b5d194f8778faacae7ee1640471d8d0b1b96c66e
pavlvs <pavlvsxavier@gmail.com>
2021-09-17 10:30:37 +0100

    generated random id for creating a user

--- repositories/users.js
+++ repositories/users.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const crypto = require('crypto')
 
 class UsersRepository {
     constructor(filename) {
@@ -22,6 +23,7 @@ class UsersRepository {
     }
 
     async create(attrs) {
+        attrs.id = this.randomId()
         const records = await this.getAll()
         records.push(attrs)
 
@@ -33,6 +35,10 @@ class UsersRepository {
             this.filename,
             JSON.stringify(records, null, 2)
         )
+    }
+
+    randomId() {
+        return crypto.randomBytes(4).toString('hex')
     }
 }
 
