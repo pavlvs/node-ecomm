8fe5358d058d6a7a70df5fed3640f3bef4083e80
pavlvs <pavlvsxavier@gmail.com>
2021-09-17 10:01:53 +0100

    added writeAll method

--- repositories/users.js
+++ repositories/users.js
@@ -24,7 +24,15 @@ class UsersRepository {
     async create(attrs) {
         const records = await this.getAll()
         records.push(attrs)
-        await fs.promises.writeFile(this.filename, JSON.stringify(records))
+
+        await this.writeAll(records)
+    }
+
+    async writeAll(records) {
+        await fs.promises.writeFile(
+            this.filename,
+            JSON.stringify(records, null, 2)
+        )
     }
 }
 
