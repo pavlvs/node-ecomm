58c5a3fc6180088ea643c97c02c7a6117d7a4b6f
pavlvs <pavlvsxavier@gmail.com>
2021-09-19 10:00:25 +0100

    exposed public directory to allow linking to css file

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cookieSession = require('cookie-session')
 const authRouter = require('./routes/admin/auth')
+
 const app = express()
 
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(
     cookieSession({
