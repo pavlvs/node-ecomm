6ae6c73d46340b7bf46b38f58448a6c13ed43a73
pavlvs <pavlvsxavier@gmail.com>
2021-09-20 08:44:21 +0100

    styled new product form

--- views/admin/products/new.js
+++ views/admin/products/new.js
@@ -4,15 +4,32 @@ const { getError } = require('../../helpers')
 module.exports = ({ errors }) => {
     return layout({
         content: /*html*/ `
-            <form action="" method="post" enctype="multipart/form-data">
-                <input type="text" placeholder="Title" name="title" />
-                ${getError(errors, 'title')}
-                <input type="text" placeholder="Price" name="price" />
-                ${getError(errors, 'price')}
-                <input type="file" name="image" id="" />
-                <button>Submit</button>
+      <div class="columns is-centered">
+        <div class="column is-half">
+          <h1 class="subtitle">Create a Product</h1>
 
-            </form>
-        `,
+          <form method="POST" enctype="multipart/form-data">
+            <div class="field">
+              <label class="label">Title</label>
+              <input class="input" placeholder="Title" name="title">
+              <p class="help is-danger">${getError(errors, 'title')}</p>
+            </div>
+
+            <div class="field">
+              <label class="label">Price</label>
+              <input class="input" placeholder="Price" name="price">
+              <p class="help is-danger">${getError(errors, 'price')}</p>
+            </div>
+
+            <div class="field">
+              <label class="label">Image</label>
+              <input type="file" name="image" />
+            </div>
+            <br />
+            <button class="button is-primary">Create</button>
+          </form>
+        </div>
+      </div>
+    `,
     })
 }
